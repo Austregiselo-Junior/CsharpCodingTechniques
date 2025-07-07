091aa207345bd4fe8a1e6ea5e01e4dc11981ac4d
Austregiselo Junior <austregiselosvj@gmail.com>
2025-07-07 18:59:04 -0300

    Delegado: Subindo exemplo 2

---
+++ Delegates/Delegates/Example2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Delegates
+{
+    public delegate bool Callback(int number);
+    internal class Example2
+    {
+        public static IEnumerable<int> Filter(List<int> numbers, Callback callback) // Se o retorno for true, o número será incluído na lista filtrada
+        {
+            foreach (var item in numbers)
+            {
+                if (callback(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static bool IsOdd(int number) => number % 2 != 0 ? true : false; // retorna true se um número for ímpar, (Tem que ter o mesmo parâmetro que o delegado Callback).
+        public static bool IsEvent(int number) => number % 2 == 0; // retorna true se um número for par, (Tem que ter o mesmo parâmetro que o delegado Callback)
+
+    }
+}
--- Delegates/Delegates/Program.cs
+++ Delegates/Delegates/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Delegates
 {
@@ -16,6 +17,28 @@ namespace Delegates
 
             Example1.Greeting greetinhWithoutInvoke = Example1.SayHi; // Você também pode atribuir o método diretamente ao delegado
             greetinhWithoutInvoke("John"); // E chamar o método diretamente, sem o Invoke()
+
+
+            Console.WriteLine("// ----------------------- Exemplo 2 ------------------------//");
+
+            var numbers = new List<int> { 1, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var evenNumbers = Example2.Filter(numbers, Example2.IsEvent); // Filtra os números pares
+
+            Console.WriteLine("Even numbers:");
+            foreach (var number in evenNumbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            var oddNumbers = Example2.Filter(numbers, Example2.IsOdd); // Filtra os números ímpares
+            Console.WriteLine("Odd numbers:");
+            foreach (var number in oddNumbers)
+            {
+                Console.WriteLine(number);
+            }
+            // OBS: Observe que o Filter pode receber o IsEvent() e o Is Odd() porque recebe um delegado como parâmetro onde esses métodos têm a mesma assinatura que o delegado.
+            Console.ReadLine();
         }
     }
 }
