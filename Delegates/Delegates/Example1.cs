59f08b96f4b9bff735750bf398712afb011e25b9
Austregiselo Junior <austregiselosvj@gmail.com>
2025-07-07 18:38:42 -0300

    Delegates: Subindo exemplo 1

---
+++ Delegates/Delegates/Example1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Delegates
+{
+    internal class Example1
+    {
+        public delegate void Greeting(string message);
+
+        public static void SayHi(string name) // Esse método deve ter a mesma assinatura do Greeting
+        {
+            Console.WriteLine($"Hi {name}");
+        }
+    }
+}
--- Delegates/Delegates/Program.cs
+++ Delegates/Delegates/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Delegates
 {
@@ -10,6 +6,16 @@ namespace Delegates
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("// ----------------------- Exemplo 1 ------------------------//");
+
+            //Para chamar o SayHi() por meio do Greeting você cria uma instância do Greeting com o SayHi()
+            //como argumento e chama o Invoke() da instância do delegado assim:
+
+            Example1.Greeting greeting = new Example1.Greeting(Example1.SayHi);
+            greeting.Invoke("John"); // Com o Invoke() você chama o método SayHi passando o argumento "John"
+
+            Example1.Greeting greetinhWithoutInvoke = Example1.SayHi; // Você também pode atribuir o método diretamente ao delegado
+            greetinhWithoutInvoke("John"); // E chamar o método diretamente, sem o Invoke()
         }
     }
 }
