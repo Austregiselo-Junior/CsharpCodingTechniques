4b36dd1d599c9fe787fca8455370205b8832dc71
Austregiselo Junior <austregiselosvj@gmail.com>
2025-07-08 15:10:58 -0300

    AnonymousMethods: Update exemaple.

--- AnonymousMethods/Program.cs
+++ AnonymousMethods/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AnonymousMethods
 {
     internal class Program
     {
+        delegate int Transform(int x);
+        static int j = 10;
+
+        /// <summary>
+        /// Mesma assinatura do delegate Transforme
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        static int Square(int x)
+        {
+            return x * x;
+        }
+
         static void Main(string[] args)
         {
+            // Método anônimo é um método sem nome que é atribuído a um delegado, pode ser de forma explicita setando o método.
+
+            Console.WriteLine("---------- Forma explícita ---------");
+
+            Transform t1 = Square;
+            Console.WriteLine(t1(10));
+            Console.ReadLine();
+
+            Console.WriteLine("---------- Forma implícita ---------");
+
+            Transform t2 = delegate (int x)
+            {
+                return x * j;
+            }; // O método anônimo não tem nome, mas é atribuído a um delegate Transform.
+
+            Console.WriteLine(t2(50));
+            Console.ReadLine();
         }
     }
 }
