e5b7e307ea9bf1174a80ef7fe72267b53106daca
Austregiselo Junior <austregiselosvj@gmail.com>
2025-07-09 15:45:48 -0300

    LINQ: Update example.

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LINQ
 {
@@ -10,6 +8,22 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
+            var person = new List<Person>
+            {
+                new Person { Name = "Alice", Age = 30 },
+                new Person { Name = "Bob", Age = 25 },
+                new Person { Name = "Charlie", Age = 15 },
+                new Person { Name = "Junior", Age = 1 },
+                new Person { Name = "Ludimila", Age = 91 },
+            };
+
+            var ofLegalAge = person.Where(p => p.Age >= 18).OrderBy(p => p.Name).Select(p => p.Name);
+
+
+            foreach (var name in ofLegalAge)
+            {
+                Console.WriteLine($"Person with legal age: {name}");
+            }
         }
     }
 }
